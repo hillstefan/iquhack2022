097a9cae63cecc274428474686df6bfb86b8fc44
Tobias Offermann <tobiasof@student.chalmers.se>
2022-01-30 15:29:38 +0100

    added final_2q (just 1 basis per person and removed swap)

--- src/quantum/Circuits.py
+++ src/quantum/Circuits.py
@@ -29,3 +29,18 @@ class Circuits:
         if receiver_base_b == 1: qasm_string += "\nH q[1]"
         qasm_string += "\nmeasure_all"
         return qasm_string
+
+    @staticmethod
+    def final_2q(key_a: int,
+                 base_a: int,
+                 key_b: int,
+                 base_b: int) -> str:
+        qasm_string = '''version 1.0\nqubits 2'''
+        if key_a == 1: qasm_string += "\nX q[0]"
+        if key_b == 1: qasm_string += "\nX q[1]"
+        if base_a == 1: qasm_string += "\nH q[0]"
+        if base_b == 1: qasm_string += "\nH q[1]"
+        if base_a == 1: qasm_string += "\nH q[1]"
+        if base_b == 1: qasm_string += "\nH q[0]"
+        qasm_string += "\nmeasure_all"
+        return qasm_string
